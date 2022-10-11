8585eed86bb4bd8ae6b2b6c2b845f666b8b2f634
Yassir Ah <yassirnugroho48@gmail.com>
2022-10-11 10:03:38 +0700

    change  font to roboto

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', function () {
+    return view('layouts/dashboard-test');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
